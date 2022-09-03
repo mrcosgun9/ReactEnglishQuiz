dbcc84b2fb05f3579ca52a98b911ced857cca776
mrcosgun <muhammetrasit.cosgun@reateknoloji.com>
2022-09-03 21:48:38 +0300

    .

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { FaGithub } from 'react-icons/fa';
 function Menu(props: { start: () => void }) {
   return (
     <div className='menu'>
       <h1>Computer Questions</h1>
-      <h3>Description</h3>
-      <button className='btn btn-sucess' onClick={() => props.start()}>Start Quizz</button>
+      <div className='flex justify-center py-2'>
+        <a href='https://github.com/mrcosgun9' target="_blank">
+          <FaGithub className='text-3xl transition duration-150 ease-in-out text-blue-500 hover:text-blue-700' />
+        </a>
+
+      </div>
+      <button className='btn btn-sucess mt-2' onClick={() => props.start()}>Start Quizz</button>
     </div>
   )
 }
